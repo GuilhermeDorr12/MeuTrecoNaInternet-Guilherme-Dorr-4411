9e04d2fba47c715dd0880fec0218ae2b9dfd3692
GuilhermeDorr12 <69913095+GuilhermeDorr12@users.noreply.github.com>
2020-08-20 14:51:03 -0300

    Teste
    Esse commit contém o teste final do sensor de chuva, possui a mesma lógica

--- Chuva_Sensor/src/main.cpp
+++ Chuva_Sensor/src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
 
+int Led = 13;
+int sensor_chuva = 34;
+int chuva = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(Led, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  chuva = analogRead(sensor_chuva);        
+  if(chuva<750)
+  {                
+    digitalWrite(Led,LOW);
+  }
+  else
+  {                    
+    digitalWrite(Led,HIGH);
+  }
+  delay(10);  
 }
